fb95a4fb65387f211dc9b5103ab468ed056f2536
Grazziani Xavier <grazzianixf@gmail.com>
2023-09-17 17:54:09 -0300

    Primeiros metodos CRUD

--- src/FirebaseHelper.js
+++ src/FirebaseHelper.js
@@ -1,33 +1,68 @@
 const firebaseApp = require("firebase/app");
 const { initializeApp } = firebaseApp;
 const firebaseFirestore = require("firebase/firestore");
-const { getFirestore, collection, getDocs } = firebaseFirestore;
+const {
+	getFirestore,
+	collection,
+	getDocs,
+	getDoc,
+	doc,
+	addDoc,
+	updateDoc,
+	deleteDoc,
+	Timestamp,
+} = firebaseFirestore;
 
 module.exports = class FirebaseHelper {
-   #app = null;
-   #firestore = null;
+	#app = null;
+	#firestore = null;
 
 	constructor(firebaseConfig) {
-      this.#app = initializeApp(firebaseConfig);
-      this.#firestore = getFirestore(this.#app);      
+		this.#app = initializeApp(firebaseConfig);
+		this.#firestore = getFirestore(this.#app);
 	}
 
-	async getAll(collectionName) {
-		const resultCollection = collection(this.#firestore, collectionName);
-		const snapshot = await getDocs(resultCollection);
+	getAll = async (collectionName) => {
+		const collectionRef = collection(this.#firestore, collectionName);
+		const snapshot = await getDocs(collectionRef);
 		const resultList = snapshot.docs.map((doc) => ({
 			id: doc.id,
-			data: doc.data(),
+			// ref: doc.ref,
+			...doc.data(),
 		}));
 
 		return resultList;
 	}
 
-   get app() {
-      return this.#app;
-   }
+	getById = (collectionName, id) => {
+		const docRef = this.getDocRefById(collectionName, id);
+		return getDoc(docRef).then((doc) => ({ id: doc.id, ...doc.data() }));
+	};
 
-   get firestore() {
-      return this.#firestore;
-   }   
+	getDocRefById = (collectionName, id) => doc(this.#firestore, collectionName, id);	
+
+	post = (collectionName, obj) =>
+		addDoc(collection(this.#firestore, collectionName), {
+			...obj,
+			created: Timestamp.now(),
+		});
+
+	put = (collectionName, obj) => {
+		const { id, ...rest } = obj;
+		const docRef = this.getDocRefById(collectionName, id);
+		return updateDoc(docRef, { ...rest, updated: Timestamp.now() });
+	};
+
+	delete = (collectionName, id) => {
+		const docRef = this.getDocRefById(collectionName, id);
+		return deleteDoc(docRef);
+	}
+
+	get app() {
+		return this.#app;
+	}
+
+	get firestore() {
+		return this.#firestore;
+	}
 };
